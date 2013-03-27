69ea0bbaf597e708285f82d8ad6f8efca803838b
John Evans <lgastako@gmail.com>
2013-03-27 15:16:36 -0700

    Add tests for simple rules.

--- test_mon.py
+++ test_mon.py
@@ -3,8 +3,47 @@ from mon import parse_rules
 
 class TestParseRules:
 
-    def test_empty_rules(self):
+    def test_zero(self):
         config = {"rules":{}}
         rules = parse_rules(config)
         assert rules == []
+
+    def test_one_rule_one_action(self):
+        test_pattern = "README.txt"
+        test_action = "echo 'README updated!'"
+
+        config = {"rules":{test_pattern: test_action}}
+        rules = parse_rules(config)
+
+        assert len(rules) == 1
+        rule = rules[0]
+
+        assert len(rule.patterns) == 1
+        pattern = rule.patterns[0]
+        assert pattern == test_pattern
+
+        assert len(rule.actions) == 1
+        action = rule.actions[0]
+        assert action == test_action
+
+    def test_one_rule_multiple_actions(self):
+        test_pattern = "README.txt"
+        test_actions = [
+            "echo 'README updated!'",
+            "echo 'I\\'m so exicted and I just can\\'t hide it!'"
+        ]
+
+        config={"rules":{test_pattern: test_actions}}
+        rules = parse_rules(config)
+
+        assert len(rules) == 1
+        rule = rules[0]
+
+        assert len(rule.patterns) == 1
+        pattern = rule.patterns[0]
+        assert pattern == test_pattern
+
+        assert len(rule.actions) == 2
+        assert rule.actions[0] == test_actions[0]
+        assert rule.actions[1] == test_actions[1]
 
